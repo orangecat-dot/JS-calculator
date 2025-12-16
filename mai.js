7f94053b37e2250e8ffe47e74da87fc98f0165c8
orangecat-dot <phdzung.ng@gmail.com>
2025-12-16 23:17:01 +0100

    Completed all the functionality of Calculator

--- mai.js
+++ mai.js
@@ -1,19 +1,79 @@
-const inputDisplay = document.getElementById("InputDisplay");
-const outputDisplay = document.getElementById("outputDisplay");
+const upperScreen = document.getElementById("upperScreen");
+const lowerScreen = document.getElementById("lowerScreen");
+const mainOps = document.querySelectorAll("#mainOp");
+
+let numA ="";
+let numB ="";
+let operator="";
 
 function appendtoDisplay(input) {
-    inputDisplay.value += input;
+    lowerScreen.value += input;
+    
+
+    if (!operator) {
+        upperScreen.value = lowerScreen.value;
+        numA = lowerScreen.value;
+
+    } else {
+        numB = lowerScreen.value;
+        upperScreen.value = `${numA}${operator}${numB}`;
+    }
 }
 
+mainOps.forEach(button => button.addEventListener('click', () => {
+    if (!lowerScreen.value) return;
+
+    operator = button.textContent;
+    lowerScreen.value ="";
+    upperScreen.value = `${numA} ${operator}`;
+
+}))
+
 function clearDisplay() {
-    inputDisplay.value = inputDisplay.value.slice(0, -1);
+    lowerScreen.value = lowerScreen.value.slice(0, -1);
+    upperScreen.value = upperScreen.value.slice(0, -1);
 
 }
 
 function allclearDisplay() {
-    inputDisplay.value ="";
+    operator ="";
+    lowerScreen.value ="";
+    upperScreen.value ="";
+    numA ="";
+    numB ="";
+}
+
+function getPercentage() {
+    lowerScreen.value = Number(lowerScreen.value)/100;
+    upperScreen.value = lowerScreen.value;
+    numA = lowerScreen.value;
+
+}
+
+function getPositiveNegative() {
+
 }
 
 function calculate() {
+    let result ="";
+    switch(operator) {
+        case '+':
+            result = Number(numA) + Number(numB)
+            break;
+        case '-':
+            result = Number(numA) - Number(numB)
+            break;
+        case 'x':
+            result = Number(numA)*Number(numB)
+            break;
+        case '/':
+            result = Number(numA) / Number(numB)
+            break;
+    }
 
+    numA = result;
+    numB="";
+    lowerScreen.value = result;
+    
 }
+
